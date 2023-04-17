7087a8336e959ac77953006df4ba585e6b522c08
Ronilson Rodrigues <g.ronilsongomes@gmail.com>
2023-04-16 23:37:30 -0300

    Atualizacao Projeto

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ronilson Gomes Rodrigues on 27/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
